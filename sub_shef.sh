7f3f72163742b401da14a2fe7f8be6a564270d74
acr24wz acr24wz <acr24wz@login2.pri.stanage.alces.network>
2025-02-18 21:55:12 +0000

    shef

---
+++ sub_shef.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --partition=gpu-h100
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --mem=82G
+#SBATCH --mail-user=wzhai2@sheffield.ac.uk
+#SBATCH --job-name=train
+
+module load cuDNN/8.7.0.84-CUDA-11.8.0
+source activate etu
+bash run_training.sh
