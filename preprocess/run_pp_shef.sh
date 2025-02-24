b352b47cd9d958e00f6028904dd1b5ee86275068
weihezhai <weihezhai@gmail.com>
2025-02-24 13:45:15 +0000

    k=10

--- preprocess/run_pp_shef.sh
+++ preprocess/run_pp_shef.sh
@@ -8,7 +8,7 @@
 # Default parameter values
 INPUT_FILE="/users/acr24wz/ETU/GNN-RAG/llm/results/KGQA-GNN-RAG-RA/rearev-lmsr/RoG-cwq/RoG/test/results_gen_rule_path_RoG-cwq_RoG_test_predictions_3_False_jsonl/False/predictions.jsonl"
 OUTPUT_FILE="/users/acr24wz/ETU/preprocess/topk_path/topk_ppl_path.jsonl"
-TOP_K=5
+TOP_K=10
 MODEL_DIR="/users/acr24wz/model/qwen2.5/7b_ori"
 
 # Parse command-line arguments
