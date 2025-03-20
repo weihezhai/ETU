d5bae6aa525c48b7e557efa3e8c84c2b0db1503e
weihezhai <weihezhai@gmail.com>
2025-03-20 15:14:38 +0000

    debug

--- rog/run_test_single_prompt.sh
+++ rog/run_test_single_prompt.sh
@@ -33,10 +33,10 @@ NC='\033[0m' # No Color
 echo -e "${BLUE}Running test with a sample from a processed file...${NC}"
 
 # Choose one of the processed files
-PROCESSED_FILE="/data/home/mpx602/projects/ETU/ETU/rog/topk_paths_prompts_rog/top8_sim_filtered_paths_with_prompts.jsonl"
+PROCESSED_FILE="/mnt/parscratch/users/acr24wz/ETU/rog/topk_paths_prompts_rog/top8_sim_filtered_paths_with_prompts.jsonl"
 
-# Test the first sample (index 0)
-python /data/home/mpx602/projects/ETU/ETU/rog/test_single_prompt.py \
+  # Test the first sample (index 0)
+  python /mnt/parscratch/users/acr24wz/ETU/rog/test_single_prompt.py \
   --model "$MODEL_PATH" \
   --input-file "$PROCESSED_FILE" \
   --sample-index 0
@@ -48,7 +48,7 @@ echo ""
 echo -e "${BLUE}Running test with a different sample index...${NC}"
 
 # Test sample at index 5
-python /data/home/mpx602/projects/ETU/ETU/rog/test_single_prompt.py \
+python /mnt/parscratch/users/acr24wz/ETU/rog/test_single_prompt.py \
   --model "$MODEL_PATH" \
   --input-file "$PROCESSED_FILE" \
   --sample-index 5
@@ -68,7 +68,7 @@ Lou Seal -> mascot.team -> sports_team.championships -> 2012 World Series
 Question:
 Lou Seal is the mascot for the team that last won the World Series when?"
 
-python /data/home/mpx602/projects/ETU/ETU/rog/test_single_prompt.py \
+python /mnt/parscratch/users/acr24wz/ETU/rog/test_single_prompt.py \
   --model "$MODEL_PATH" \
   --prompt "$PROMPT"
 
