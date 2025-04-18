722e485febf3a1e581b16cd0471c918f0822bb7d
weihezhai <weihezhai@gmail.com>
2025-04-18 15:51:14 +0100

    ner

--- graph_to_path/process_subgraph_ner.sh
+++ graph_to_path/process_subgraph_ner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --partition=gpu
-#SBATCH --qos=gpu
-#SBATCH --gres=gpu:1
+##SBATCH --partition=gpu
+##SBATCH --qos=gpu
+##SBATCH --gres=gpu:1
 #SBATCH --mem=82G
 #SBATCH --cpus-per-task=12
 #SBATCH --mail-user=wzhai2@sheffield.ac.uk
