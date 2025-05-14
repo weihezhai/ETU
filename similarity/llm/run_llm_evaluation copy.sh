95dda3428ab585e2dc0fc845d4c744fc27f5877e
weihezhai <weihezhai@gmail.com>
2025-05-14 20:23:35 +0100

    a

--- similarity/llm/run_llm_evaluation copy.sh
+++ similarity/llm/run_llm_evaluation copy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=gpu-h100
+#SBATCH --partition=gpu
 #SBATCH --qos=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=82G
@@ -8,8 +8,8 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --job-name=llm_evaluation
 #SBATCH --output=./logs/llm_evaluation_%A_%a.out
-#SBATCH --time=0-10:00:00
-
+#SBATCH --time=0-24:00:00
+#SBATCH --array=0-8  # Process 9 files (indices 0-8)
 
 # Load necessary modules
 module load Anaconda3/2024.02-1
@@ -30,22 +30,26 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 # Create a directory for results
-mkdir -p /mnt/parscratch/users/acr24wz/ETU/__webqsp__/icl/llm_gen_results
+mkdir -p /mnt/parscratch/users/acr24wz/ETU/similarity/topsim/evaluation_results
 
 # Get all prompt files
-
+FILES=($(ls /mnt/parscratch/users/acr24wz/ETU/similarity/topsim/topk_paths_prompts/top*_sim_filtered_paths_with_prompts.jsonl))
 
 # Get the specific file for this array task
-input="/mnt/parscratch/users/acr24wz/ETU/__webqsp__/icl/icl_prompt/web_filtered_paths_prompt.jsonl"
-output="/mnt/parscratch/users/acr24wz/ETU/__webqsp__/icl/llm_gen_results/llm_gen_results.json"
+file=${FILES[$SLURM_ARRAY_TASK_ID]}
+filename=$(basename "$file")
+base_filename="${filename%_with_prompts.jsonl}"
+output="/mnt/parscratch/users/acr24wz/ETU/similarity/topsim/evaluation_results/${base_filename}_llm_results.json"
+
+echo -e "${BLUE}Task ID: $SLURM_ARRAY_TASK_ID - Evaluating $filename...${NC}"
 
 # Optional: limit the number of samples for testing
 # MAX_SAMPLES="--max-samples 10"
 MAX_SAMPLES=""
 
-python /mnt/parscratch/users/acr24wz/ETU/__webqsp__/icl/llm_gen_results/test_prompts_with_llm.py \
+python /mnt/parscratch/users/acr24wz/ETU/similarity/llm/test_prompts_with_llm.py \
   --model "$MODEL_PATH" \
-  --input "$input" \
+  --input "$file" \
   --output "$output" \
   --batch-size 1 \
   $MAX_SAMPLES
